37438ae672d051289963fe43225a9703322eaadf
Siarhei <62071543+SiarheiYurevich@users.noreply.github.com>
2020-09-30 23:57:43 +0300

    Add files via upload

---
+++ by/java_intro_online/mod03/task01_05_string_as_char_array/Task02.java
@@ -0,0 +1,26 @@
+package by.java_intro_online.mod03.task01_05_string_as_char_array;
+
+//Replace all occurrences of "word" in a string with "letter"
+
+public class Task02 {
+
+	public static void main(String[] args) {
+		
+	//	String str = "";
+		
+		
+		
+	}
+	
+	public static String replaceWordWithLetter(String string) {
+		
+		
+		String newString = "";
+		
+		
+		
+		
+		return newString;
+	}
+	
+}
