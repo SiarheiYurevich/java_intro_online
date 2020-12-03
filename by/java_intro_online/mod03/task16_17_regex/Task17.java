49a4e93d925d2f0a001a3f16a3e57d21a6247898
Siarhei <62071543+SiarheiYurevich@users.noreply.github.com>
2020-12-04 00:57:59 +0300

    Update Task17.java

--- by/java_intro_online/mod03/task16_17_regex/Task17.java
+++ by/java_intro_online/mod03/task16_17_regex/Task17.java
@@ -3,23 +3,7 @@ package by.java_intro_online.mod03.task16_17_regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/* Given the string contains following text (XML document):
- * 
- * <notes>
- * 	 <note id = '1'>
- * 		<to>Vasily</to>
- * 		<from>Sveta</from>
- * 		<heading>Reminder</heading>
- * 		<body>Call me tomorrow!</body>
- * 	 </note>
- *   <note id = '2'>
- * 		<to>Petr</to>
- * 		<from>Maria</from>
- * 		<heading>Important reminder</heading>
- * 		</body>
- * 	 </note>
- * </notes>
- * 
+/* Given the string contains following text (XML document).
  * Write parser that alloys you consistently return content of XML nodes and its type 
  * (start tag, end tag, content, empty tag).
  * Do not use library XML parsers. 
@@ -85,69 +69,6 @@ public class Task17 {
 		return note.toString();
 		
 	}
-	
-/*
-	// notes reading
-	public static String readNotes(String inputXmlNotesDoc) {
-
-		StringBuilder outputNotes = new StringBuilder();
-
-		// reading note id
-
-		Pattern patternId = Pattern.compile("<note id = '[0-9]+'>");
-		Matcher matcherId = patternId.matcher(inputXmlNotesDoc);
-		String id = "note id ";
-		int idBegin = 12;
-		int idEnd = 2;
-		int idLength = 0;
-
-		if (matcherId.find()) {
-			idLength = matcherId.group().length() - idEnd;
-			id += matcherId.group().substring(idBegin, idLength) + "/n";
-			outputNotes.append(id);
-		}
-
-		// recipient
-		Pattern patternRecipient = Pattern.compile("<to>[a-zA-Z]+</to>");
-		Matcher matcherRecipient = patternRecipient.matcher(inputXmlNotesDoc);
-		String recipient = "recipient ";
-		int recBegin = 4;
-		int recEnd = 5;
-		int recLength = 0;
-
-		if (matcherRecipient.find()) {
-			recLength = matcherRecipient.group().length() - recEnd;
-			recipient += matcherRecipient.group().substring(recBegin, recLength) + "/n";
-			outputNotes.append(recipient);
-		}
-
-		Pattern patternName = Pattern.compile("<name>[a-zA-Z]+</name>");
-		Matcher matcherName = patternName.matcher(text);
-		int nameBegin = 6;
-		int nameEnd = 0;
-
-		Pattern patternTP = Pattern.compile("<price>[0-9]+</price>");
-		Matcher matcherTP = patternTP.matcher(text);
-		int priceBegin = 7;
-		int priceEnd = 0;
-		int price = 0;
-
-		while (matcherName.find()) {
-
-			nameEnd = matcherName.group().length() - 7;
-			String name = matcherName.group().substring(nameBegin, nameEnd);
-
-			if (matcherTP.find()) {
-
-				priceEnd = matcherTP.group().length() - 8;
-				price = Integer.parseInt(matcherTP.group().substring(priceBegin, priceEnd));
-			}
-
-			Treasure treasure = new Treasure(name, price);
-			cave.addTreasure(treasure);
-		}
-		return xmlOutputNotesDoc;
-	}*/
 
 	public static String readTag(String xmlText, String tagName) {
 
